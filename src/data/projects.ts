a3b975bc610582cf618e1f22dd45b1355cc29191
simurgh420 <52625819+simurgh420@users.noreply.github.com>
2025-10-10 23:14:04 +0330

    fix project shop

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,32 +1,31 @@
-import type { ProjectItem } from "@/lib/types";
+import type { ProjectItem } from '@/lib/types';
 
 export const projects: ProjectItem[] = [
-      {
+  {
     id: 1,
-        title: "CMS React",
-    description: " داشبورد ادمین مدرن (Modern Admin Dashboard",
-        demo: "https://cms-5241.vercel.app/",
-        source: "https://github.com/simurgh420/CMS",
-      image: "images/dashboard-preview.png",
-    icon: "🎨", 
-    },
-    {
-        id: 2,
-        title: "DGShop",
-    description: " DGShop – Ecommerce Sample Project",
-        demo: "https://dgshap.vercel.app/",
-        source: "https://github.com/simurgh420/dgshapS",
-        image: "images/DGShop.png",
-    icon: "🛒",
-    },
-    {
-        id: 3,
-        title: "CMS Nextjs",
-        description: "CMSNextP2 – پنل مدیریت فارسی (RTL) با Next.js",
-        demo: "https://cmsnextp2.vercel.app/",
-        source: "https://github.com/simurgh420/cmsnextp2",
-          image: "images/dashboard.png",
-        icon: "📝",
-    },
-    
-]
+    title: 'CMS React',
+    description: ' داشبورد ادمین مدرن (Modern Admin Dashboard',
+    demo: 'https://cms-5241.vercel.app/',
+    source: 'https://github.com/simurgh420/CMS',
+    image: 'images/dashboard-preview.png',
+    icon: '🎨',
+  },
+  {
+    id: 2,
+    title: 'DGShop',
+    description: ' DGShop – Ecommerce Sample Project',
+    demo: 'https://dgshap.vercel.app/',
+    source: 'https://github.com/simurgh420/dgshap',
+    image: 'images/DGShop.png',
+    icon: '🛒',
+  },
+  {
+    id: 3,
+    title: 'CMS Nextjs',
+    description: 'CMSNextP2 – پنل مدیریت فارسی (RTL) با Next.js',
+    demo: 'https://cmsnextp2.vercel.app/',
+    source: 'https://github.com/simurgh420/cmsnextp2',
+    image: 'images/dashboard.png',
+    icon: '📝',
+  },
+];
